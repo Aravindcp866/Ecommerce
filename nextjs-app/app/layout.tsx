323b1b00b575a2303e9362782e2ba97b84b39dae
aravind886 <aravindcp@carestack.com>
2025-02-01 14:12:08 +0530

    head tag added

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 import Loader from "./components/Loader";
 import json from './jsonld.json'
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -25,12 +26,14 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="bg-white text-black">
-      <title>United by Vibe - Trendy T-Shirts in India | Best T-shirt Brand in Kerala</title>
-      <meta name="description" content="United by Vibe (UBV) is India's best T-shirt brand, offering stylish and trendy graphic T-shirts with premium quality. Order online today!"></meta>
-      <script type="application/ld+json">
-        {JSON.stringify(json)}
-      </script>
-      <meta name="google-site-verification" content="tqcf21sne-RWs8PzP2nacZx5C8qo0sESclDkktGRC5Q" />
+      <Head>
+        <title>United by Vibe - Trendy T-Shirts in India | Best T-shirt Brand in Kerala</title>
+        <meta name="description" content="United by Vibe (UBV) is India's best T-shirt brand, offering stylish and trendy graphic T-shirts with premium quality. Order online today!"></meta>
+        <script type="application/ld+json">
+          {JSON.stringify(json)}
+        </script>
+        <meta name="google-site-verification" content="tqcf21sne-RWs8PzP2nacZx5C8qo0sESclDkktGRC5Q" />
+      </Head>
       <body className="font-sans ">
         <section className="w-full h-[100vh]">
           <Header />
