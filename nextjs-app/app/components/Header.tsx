f8e8e0c284fe57f3d69296fdea0d33a497296491
Akash <akashas@carestack.com>
2025-02-02 00:25:42 +0530

    major tweak in header

--- nextjs-app/app/components/Header.tsx
+++ nextjs-app/app/components/Header.tsx
@@ -4,9 +4,14 @@ import Link from "next/link";
 import logo from "../public/images/logo.png"
 import Image from "next/image";
 import CTA from "./Cta";
-import Head from "next/head";
-import { json } from "../jsonld";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import MobileSidebar from "./common/MobileSidebar";
 
+
+export const menu = [{ heading: 'Home', route: '/' },
+  { heading: 'About', route: '/about' },
+  { heading: 'Contact', route: '/contact' }
+  ]
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -14,42 +19,25 @@ const Header = () => {
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
-  const menu = [{ heading: 'Home', route: '/' },
-  { heading: 'About', route: '/about' },
-  { heading: 'Contact', route: '/contact' }
-  ]
+
 
   return (
     <header className="shadow-md bg-yellow-gradient">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
-          <Image src={logo} alt={"Trendy T-shirts in India"} width={90} height={90} />
+          {/* <Image src={logo} alt={"Trendy T-shirts in India"} width={90} height={90} /> */}
+
+         <MobileSidebar/>
+
           <p className="font-bold">UBV(Unitied By Vibe)
           </p>
           <div className="text-2xl font-bold text-gray-800">
             {/* <Link href="/">Logo</Link> */}
+            <Link href="/signup" className="flex flex-col items-center">
+            <HiOutlineUserCircle />
+            </Link>
           </div>
-          <div className="sm:hidden">
-            <button
-              onClick={toggleMobileMenu}
-              className="text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
+   
 
           {/* Desktop Navigation */}
           <nav className="hidden sm:flex space-x-6">
@@ -63,12 +51,15 @@ const Header = () => {
               })
             }
             <CTA value={"Buy Now  "} />
-          </nav>
+          </nav>.
+
         </div>
 
         {/* Mobile Menu */}
-        {isMobileMenuOpen && (
-          <div className="sm:hidden flex flex-col space-y-2 bg-gray-100 p-4 rounded-md shadow-md">
+
+        {/* {isMobileMenuOpen && (
+
+          <div className="absolute sm:hidden flex flex-col space-y-2 bg-gray-100 p-4 rounded-md shadow-md">
             {
               menu?.map((e: any, i: number) => {
                 return (
@@ -78,9 +69,8 @@ const Header = () => {
                 )
               })
             }
-            {/* <CTA value={"Buy Now  "} /> */}
           </div>
-        )}
+        )} */}
       </div>
     </header>
   );
