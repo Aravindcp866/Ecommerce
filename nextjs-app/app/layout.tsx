5d3cbe4340e7c47d6aa9131a1bb9d183c35ec32c
aravind886 <aravindcp@carestack.com>
2025-02-02 11:43:54 +0530

    build fix

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -1,19 +1,13 @@
-"use client"
+"use client";
 import Header from "@/app/components/Header";
 import "./globals.css";
-
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 import Loader from "./components/Loader";
 import { json } from "./jsonld";
 import { DataProvider } from "./components/Context/GlobalContext";
 
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState(false);
   const pathname = usePathname();
 
@@ -23,26 +17,25 @@ export default function RootLayout({
     return () => clearTimeout(timeout);
   }, [pathname]);
 
-
-
   return (
     <html lang="en" className="bg-white text-black">
       <head>
         <title>United by Vibe - Trendy T-Shirts in India | Best T-shirt Brand in Kerala</title>
-        <meta name="description" content="United by Vibe (UBV) is India's best T-shirt brand, offering stylish and trendy graphic T-shirts with premium quality. Order online today!"></meta>
-        <script type="application/ld+json">
-          {JSON.stringify(json)}
-        </script>
+        <meta
+          name="description"
+          content="United by Vibe (UBV) is India's best T-shirt brand, offering stylish and trendy graphic T-shirts with premium quality. Order online today!"
+        />
+        <script type="application/ld+json">{JSON.stringify(json)}</script>
         <meta name="google-site-verification" content="tqcf21sne-RWs8PzP2nacZx5C8qo0sESclDkktGRC5Q" />
       </head>
-      <body className="font-sans ">
-       
-
-          <DataProvider children={<section className="w-full h-[100vh]">
+      <body className="font-sans">
+        <DataProvider>
+          <section className="w-full h-[100vh]">
             {['/signup', '/login'].includes(pathname) ? null : <Header />}
             {children}
             {loading && <Loader />}
-          </section>} />
+          </section>
+        </DataProvider>
       </body>
     </html>
   );
