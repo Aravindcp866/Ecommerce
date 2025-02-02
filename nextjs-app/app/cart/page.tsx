05fcd529f74f2c86ab4cbb041b3eea29c079a1df
aravind886 <aravindcp@carestack.com>
2025-02-02 11:47:17 +0530

    debugger removal

--- nextjs-app/app/cart/page.tsx
+++ nextjs-app/app/cart/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { EmptyCart } from "../components/CartComponent/EmptyCart";
 import { useData } from "../components/Context/GlobalContext";
 
 export default function CartPage() {
@@ -6,10 +7,9 @@ export default function CartPage() {
 
   console.log({ bag })
 
-  return <div className="flex items-center  flex-col gap-4 pt-5 px-14">
-
-    <i className="fa-solid fa-cart-shopping w-10 h-10"></i>
-    <h1 className="text-2xl text-black font-bold">Your Cart Is Empty!</h1>
-    <h4 className="font-semibold text-lg ">When you add products, they will appear here.</h4>
-  </div>;
+  return (<div>
+    {
+      !bag?.length && <EmptyCart />
+    }
+  </div>)
 }
